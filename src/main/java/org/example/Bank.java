5e6ea34a0ee835da52e6191b56ecb6849c3c3d0c
админ <slava.gusaov@gmail.com>
2023-09-21 21:44:19 +0300

    Initial commit

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Bank implements BankUser {
+    private String userName;
+    private int accountId;
+    private double accountBalance;
+    private BankCard userCard;
+    private ArrayList<Regition> numberPhone;
+
+    public Bank(String userName, int accountId, double accountBalance, BankCard userCard, ArrayList<Regition> numberPhone) {
+        this.userName = userName;
+        this.accountId = accountId;
+        this.accountBalance = accountBalance;
+        this.userCard = userCard;
+        this.numberPhone = numberPhone;
+
+    }
+
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public int getAccountId() {
+        return accountId;
+    }
+
+    @Override
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    @Override
+    public BankCard getUserCard() {
+        return userCard;
+    }
+
+    @Override
+    public Regition getNumberPhone() {
+        return null;
+    }
+
+    @Override
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+}
+
+
+
